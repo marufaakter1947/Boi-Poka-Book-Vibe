6411be1ef09a299413373c5e4c1473f276a840a6
Marufa Akter <marufaakter1947@gmail.com>
2025-10-04 21:31:18 +0600

    Set up all necessary things connected

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hi f</h1>
+  },
+])
+
